9eff670b21e4f63c225d2b916c7ecabec9dc7779
float <paddingme@163.com>
2022-04-25 22:34:19 +0800

    时间轴

--- src/views/BlogList.jsx
+++ src/views/BlogList.jsx
@@ -1,75 +1,82 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
-import { Card, Empty } from "antd";
-import { HistoryOutlined, ReadOutlined } from '@ant-design/icons';
+import {Card, Empty, Spin} from "antd";
+import {HistoryOutlined, ReadOutlined} from '@ant-design/icons';
 
-function getArticleList () {
+function getArticleList() {
 	return axios.get('https://www.fastmock.site/mock/5a9f84630f3ede0293cf99c7f56c0644/blog/articleList')
 }
 
-function Blog ({ blog }) {
-	const hoverColor = { color: "rgba(0, 0, 0, 0.45)" }
-	const buttonSpace = { marginRight: "5px" }
-	const { Meta } = Card
-	const { title, content, createTime } = blog
+function Blog({blog}) {
+	const hoverColor = {color: "rgba(0, 0, 0, 0.45)"}
+	const buttonSpace = {marginRight: "5px"}
+	const {Meta} = Card
+	const {title, content, createTime} = blog
 	return (<Card
 		style={{
 			width: '100%',
 			marginBottom: '16px',
-			borderColor: "transparent",
-			cursor: "pointer",
-			boxShadow: "0 1px 2px -2px rgb(0 0 0 / 16%), 0 3px 6px 0 rgb(0 0 0 / 12%), 0 5px 12px 4px rgb(0 0 0 / 9%)"
+			cursor: "pointer"
 		}}
+		className={ 'card-box-shadow'  }
 		actions={[
 			<span style={hoverColor}>
-				<HistoryOutlined style={buttonSpace} />
-				{ createTime }
+				<HistoryOutlined style={buttonSpace}/>
+				{createTime}
 			</span>,
 			<span>
-				<ReadOutlined style={buttonSpace} />
+				<ReadOutlined style={buttonSpace}/>
 				阅读全文
 			</span>
 		]}>
-		<Meta
-			style={{
-				textAlign: "tight",
-				height: 80,
-				overflow: 'hidden',
-				textOverflow: "ellipsis",
-				display: "-webkit-box",
-				WebkitLineClamp: 2,
-				WebkitBoxOrient: "vertical"
-			}}
-			title={ title }
-			description={ content }/>
+		<p style={{ fontWeight: "bold", fontSize: "16px" }}>
+			{ title }
+		</p>
+		<div className={'overflow-dot'}>
+			{ content }
+		</div>
 	</Card>)
 }
 
-function BlogList () {
-	const [ dataList, setDataList ] = useState([])
-	const [ show, setShow ] = useState(false)
+function BlogList() {
+	const [dataList, setDataList] = useState([])
+	const [show, setShow] = useState(false)
+	const [loading, setLoading] = useState(true)
 
 	useEffect(() => {
 		getArticleList()
-			.then(({ data }) => {
+			.then(({data}) => {
 				setDataList(data.list)
 				if (data.list.length === 0) {
 					setShow(true)
 				}
+				setLoading(false)
 			})
 	}, [])
 
 	return (
-		<div>
+		<div style={{
+			position: "relative",
+			minHeight: "500px"
+		}}>
+			<Spin
+				size={'large'}
+				style={{
+					position: "absolute",
+					top: "50%",
+					left: "50%",
+					display: !loading && "none"
+				}}/>
 			{
-				dataList.map(blog => <Blog key={blog.id} blog={blog} />)
+				dataList.map(blog => <Blog key={blog.id} blog={blog}/>)
 			}
 			<Empty
 				image={Empty.PRESENTED_IMAGE_SIMPLE}
 				style={{
-				display: !show && "none"
-			}} />
+					display: !show && "none"
+				}}/>
 		</div>
 	)
 }
+
 export default BlogList
--- src/views/Time.jsx
+++ src/views/Time.jsx
@@ -1,8 +1,52 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import Timeline from "antd/es/timeline";
+import Card from "antd/es/card";
+import axios from "axios";
 
-function Time () {
+function getArticleList() {
+	return axios.get('https://www.fastmock.site/mock/5a9f84630f3ede0293cf99c7f56c0644/blog/articleList')
+}
+
+function Time() {
+	const [timeList, setTimeList] = useState([])
+	const [show, setShow] = useState(false)
+	const [loading, setLoading] = useState(true)
+
+	useEffect(() => {
+		getArticleList()
+			.then(({data}) => {
+				setTimeList(data.list)
+				if (data.list.length === 0) {
+					setShow(true)
+				}
+				setLoading(false)
+			})
+	}, [])
+
 	return (
-		<div>时间线</div>
+		<div style={{
+			paddingTop: "10px"
+		}}>
+			<Timeline mode="alternate">
+				{
+					timeList.map(({ title, content, createTime }) => (
+						<Timeline.Item label={ createTime }>
+							<Card style={{width: 300, display: "inline-block", borderRadius: 3 }}
+								  bodyStyle={{ padding: 15 }}
+								  className={ 'card-box-shadow' }>
+								<p style={{ fontWeight: "bold", fontSize: "16px" }}>
+									{ title }
+								</p>
+								<div className={'overflow-dot'}>
+									{ content }
+								</div>
+							</Card>
+						</Timeline.Item>
+					))
+				}
+			</Timeline>
+		</div>
 	)
 }
+
 export default Time
